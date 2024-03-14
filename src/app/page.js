7f34319ac5cd582cb091d62e99e0cd0c1a721f47
Josue omar catzin chacon <20393135@utcancun.edu.mx>
2024-03-14 12:51:41 -0500

    d

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 "use client";
 import { useState } from 'react';
 import 'boxicons'
-import Image from "next/image";
-import universidad from '../public/assets/universidadAssets/IMAGENES PAGINA WEB +U/+universidades.png'
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
